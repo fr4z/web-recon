b6911a2514cf666d572927ba4de92cec4c87293c
fr4z <lazaro.fraga@gmail.com>
2020-02-08 14:09:01 -0500

    version 1.0

--- recon.py
+++ recon.py
@@ -12,6 +12,7 @@ import sys
 import os
 import argparse
 import subprocess
+import shutil
 
 def checkFile(path):
     return os.path.isfile(path) and os.path.getsize(path) > 0
@@ -21,17 +22,14 @@ def prepareDir(target):
     os.makedirs(targetDir, exist_ok=True)
     screenShotDir = os.path.abspath(os.path.join(target, 'gowitness'))
     os.makedirs(screenShotDir, exist_ok=True)
-    gobusterDir = os.path.abspath(os.path.join(target, 'gobuster'))
-    os.makedirs(gobusterDir, exist_ok=True)
 
 def assetFinder(target):
     print(f'[-] Running assetfinder on {target}')
     path = os.path.abspath(os.path.join(target))
     output = open(f'{path}/assetfinder.txt', 'a')
-    result = subprocess.run(['assetfinder', target], stdout=subprocess.PIPE, text=True)
+    result = subprocess.run(['assetfinder', '--subs-only', target], stdout=subprocess.PIPE, text=True)
     output.write(result.stdout)
     output.close()
-    return (result.stdout).splitlines()
 
 def httProbe(assetPath):
     print('[-] Running HTTProbe')
@@ -40,31 +38,17 @@ def httProbe(assetPath):
     result = subprocess.run(['httprobe'], stdin=open(assetPath, 'r'), stdout=subprocess.PIPE, text=True)
     output.write(result.stdout)
     output.close()
-    return (result.stdout).splitlines()
-
-def gobuster(filePath):
-    for subdomain in subdomains:
-        print(f'[-] Running gobuster on {subdomain}')
-        result = subprocess.run(['gobuster', 'dir', '-q', '-w', 'wordlist.txt', subdomain], stdout=subprocess.PIPE, text=True)
-        if result:
-            output.write(result.stdout)
-            path = os.path.abspath(os.path.join(target,'gobuster'))
-            output = open(f'{path}/{subdomain}.txt', 'a')
-            output.close()
 
 def gowitness(filePath):
-    path = os.path.abspath(os.path.join(target,'gobuster'))
-    with open(filePath, 'r') as file:
-        subdomains = list(set(file.read().replace('\n', '').splitlines()))
-    for subdomain in subdomains:
-        print(f'[-] Running gowitness on {subdomain}')
-        result = subprocess.run(['gowitness', 'single', f'https://{subdomain}', '-d', path], stdout=subprocess.PIPE, text=True)
-        if result:
-            output.write(result.stdout)
-            output = open(f'{path}/{subdomain}.txt', 'a')
-            output.close()
+    path = os.path.abspath(os.path.join(target,'gowitness'))
+    os.chdir(path)
+    with open(filePath, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            subdomain = line.rstrip()
+            print(f'[-] Running gowitness on {subdomain}')
+            subprocess.run(['gowitness', 'single', f'--url={subdomain}'], stdout=subprocess.PIPE, text=True)
 
-    
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='A Python Web Reconaissance Tool')
     parser.add_argument('target', action='store', help='hostname (e.g. example.com example2.com) to scan.', nargs="*")
@@ -77,26 +61,20 @@ if __name__ == '__main__':
         print('You must only provide ONE target')
         sys.exit(1)
     else:
-        print(args.target)
+        target = args.target[0]
+        print(f'[-] Preparing to Scan {target}')
 
-    target = args.target[0]
-
-    print(f'[-] Preparing to Scan {target}')
     prepareDir(target)
-    subdomains = list(set(assetFinder(target)))
-    print(subdomains)
+    assetFinder(target)
 
     assetPath = f'{target}/assetfinder.txt'
     if checkFile(assetPath):
         httProbe(assetPath)
 
-    httprobePath = f'{target}/httprobe.txt'
+    path = os.path.abspath(os.path.join(target))
+    httprobePath = f'{path}/httprobe.txt'
     if checkFile(httprobePath):
-        gobuster(httprobePath)
+        gowitness(httprobePath)
     else:
         print("Looks like there's nothing left to scan")
         sys.exit()
-    
-    httprobePath = f'{target}/httprobe.txt'
-    if checkFile(httprobePath):
-        gowitness(httprobePath)
